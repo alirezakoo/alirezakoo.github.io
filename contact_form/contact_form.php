803cfb02341969cd7b663766db93a6f32e810213
a.bahramibavani <alirezakoo@gmail.com>
2021-12-06 17:20:00 +0330

    enable send mail in contact

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure
-$from = 'Contact form <yourmail@domain.com>';
-$sendTo = 'your@mail.com';
+$from = 'Contact form <alirezakoo@gmail.com>';
+$sendTo = 'alirezakoo@gmail.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
@@ -12,7 +12,7 @@ $errorMessage = 'There was an error while submitting the form. Please try again 
 
 if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
     //your site secret key
-    $secret = '6LdqmCAUAAAAANONcPUkgVpTSGGqm60cabVMVaON';
+    $secret = '6LdqmCAUAAAAAMMNEZvn6g4W5e0or2sZmAVpxVqI';
     //get verify response data
 
     $c = curl_init('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
